ac4cfac32aeb3137df6ea878666a05b6cb9b551e
Iqra Fatima <121562972+Iqra-F@users.noreply.github.com>
2024-03-11 11:16:05 +0500

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -7,12 +7,11 @@ const Login = () => {
   const [email, setEmail] = useState(" ");
   const [password, setPassword] = useState(" ");
   const { user, logIn } = UserAuth();
-  const [error, setError] = useState(" "); //if the use sets a wrong password, error state will bw changed
+  const [error, setError] = useState(" "); //if the use sets a wrong password, the error state will bw changed
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    //we don't want our page to be submitted everytime when we submit aou form,
-    // we will preventdefault and that is why we will pass an event e
+    //we don't want our page to be submitted whenever we submit our form,
     e.preventDefault();
     setError(" ");
     try {
@@ -26,14 +25,12 @@ const Login = () => {
 
   return (
     <div className="w-full h-screen">
-      {/* background image, this image will be hidden in phones i.e below small screen */}
       <img
         className="hidden sm:block w-full h-full object-cover"
         src="https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
         alt="/"
         
       />
-      {/* on phones there will be a black background */}
       <div className="bg-black/60 fixed top-0 left-0 h-screen w-full"></div>  {/*overlay*/}
       <div className="fixed w-full px-4 py-24 z-50">
         {/* form */}
